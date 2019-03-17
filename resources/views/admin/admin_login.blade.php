b1e6ae6c771dd52de47d2657d779d0aafe820787
iqbalaulia <miqbalar16@gmail.com>
2019-03-18 00:45:33 +0700

    login admin page

--- resources/views/admin/admin_login.blade.php
+++ resources/views/admin/admin_login.blade.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Ela Admin - HTML5 Admin Template</title>
+    <title>Admin</title>
     <meta name="description" content="Ela Admin - HTML5 Admin Template">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -37,14 +37,14 @@
                     </a>
                 </div>
                 <div class="login-form">
-                    <form>
+                    <form method="POST" action="">
                         <div class="form-group">
-                            <label>Email address</label>
-                            <input type="email" class="form-control" placeholder="Email">
+                            <label>Email</label>
+                            <input type="text" class="form-control" name="admin_email" placeholder="Email Address">
                         </div>
                         <div class="form-group">
                             <label>Password</label>
-                            <input type="password" class="form-control" placeholder="Password">
+                            <input type="password" class="form-control" name="admin_password" placeholder="Password">
                         </div>
                         <div class="checkbox">
                             <label>
